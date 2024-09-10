a4987297b00af13a5d3f9fe5986963268876b631
Stephen Zhou <hi@hyoban.cc>
2024-09-10 23:23:56 +0800

    chore: update lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
 import { defineConfig } from 'eslint-config-hyoban'
 
-export default defineConfig({
-  fileCase: 'kebabCase',
-  typeChecked: 'essential',
-  project: true,
-  tailwindCSS: {
-    order: false,
+export default defineConfig(
+  {
+    fileCase: 'kebabCase',
+    typeChecked: 'essential',
+    project: true,
+    tailwindCSS: {
+      order: false,
+    },
   },
-})
+  {
+    rules: {
+      '@stylistic/quotes': ['error', 'single', { allowTemplateLiterals: true, avoidEscape: true }],
+    },
+  },
+)
