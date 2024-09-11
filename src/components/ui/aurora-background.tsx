4fca4af8c2ad612cd358fa12d51618482e541ac3
Stephen Zhou <hi@hyoban.cc>
2024-09-11 22:15:48 +0800

    chore: try orange for hero bg

--- src/components/ui/aurora-background.tsx
+++ src/components/ui/aurora-background.tsx
@@ -31,7 +31,7 @@ export function AuroraBackground({
               `
             [--white-gradient:repeating-linear-gradient(100deg,var(--white)_0%,var(--white)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)]
             [--dark-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)]
-            [--aurora:repeating-linear-gradient(100deg,var(--blue-500)_10%,var(--indigo-300)_15%,var(--blue-300)_20%,var(--violet-200)_25%,var(--blue-400)_30%)]
+            [--aurora:repeating-linear-gradient(100deg,var(--orange-500)_10%,var(--red-300)_15%,var(--orange-300)_20%,var(--yellow-200)_25%,var(--orange-400)_30%)]
             [background-image:var(--white-gradient),var(--aurora)]
             dark:[background-image:var(--dark-gradient),var(--aurora)]
             [background-size:300%,_200%]
