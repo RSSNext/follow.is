9b6020118ed39584e1ef61e53b8eb4d5e79b29e8
Stephen Zhou <hi@hyoban.cc>
2024-09-13 08:51:54 +0800

    chore: update faq

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -16,29 +16,9 @@ interface FAQProps {
 
 const FAQList: FAQProps[] = [
   {
-    question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit?',
-    answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint labore quidem quam? Consectetur sapiente iste rerum reiciendis animi nihil nostrum sit quo, modi quod.',
+    question: 'What platforms are supported?',
+    answer: 'Follow Desktop for Windows, macOS, Linux, and Browser; Follow Mobile for Android and iOS (coming soon).',
     value: 'item-1',
-  },
-  {
-    question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit?',
-    answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint labore quidem quam? Consectetur sapiente iste rerum reiciendis animi nihil nostrum sit quo, modi quod.',
-    value: 'item-2',
-  },
-  {
-    question: 'Lorem ipsum dolor sit amet  Consectetur natus dolores minus quibusdam?',
-    answer: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore qui nostrum reiciendis veritatis necessitatibus maxime quis ipsa vitae cumque quo?',
-    value: 'item-3',
-  },
-  {
-    question: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit?',
-    answer: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-    value: 'item-4',
-  },
-  {
-    question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur natus?',
-    answer: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint labore quidem quam? Consectetur sapiente iste rerum reiciendis animi nihil nostrum sit quo, modi quod.',
-    value: 'item-5',
   },
 ]
 
