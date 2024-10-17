27a4776f37c129a383da76970359efcce891c7d8
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-10-17 17:36:08 +0800

    chore: update

--- src/app/airdrop/page.tsx
+++ src/app/airdrop/page.tsx
@@ -23,7 +23,7 @@ export default function AirdropPage() {
   )
 
   return (
-    <div className="min-h-screen pt-40 flex flex-col items-center dark:bg-[linear-gradient(45deg,#d43421,#ff6a0b,#ff5d07)]">
+    <div className="min-h-screen flex flex-col items-center justify-center dark:bg-[linear-gradient(45deg,#d43421,#ff6a0b,#ff5d07)]">
       <main className="w-full space-y-8">
         <div className="text-center text-7xl font-bold space-y-2">
           <p>JOIN TO SHARE</p>
