547fc7be330fcdf550e7716557dbe4b819055656
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-10-18 20:29:25 +0800

    feat: claim

--- src/app/airdrop/page.tsx
+++ src/app/airdrop/page.tsx
@@ -1,42 +1,77 @@
 'use client'
-
-import { useSession } from '@hono/auth-js/react'
+import { getCsrfToken, useSession } from '@hono/auth-js/react'
+import { toast, Toaster } from 'sonner'
 import useSWR from 'swr'
 
 import { AuthButton } from '@/components/auth'
+import { Button } from '@/components/ui/button'
 import { env } from '@/env'
 
 export default function AirdropPage() {
   const { data: session, status } = useSession()
-  const { data: amount } = useSWR(
+  const { data } = useSWR(
     session && status === 'authenticated' ? 'get-airdrop-amount' : null,
     async () => {
       const res = await fetch(`${env.NEXT_PUBLIC_API_URL}/wallets/airdrop`, {
         credentials: 'include',
       })
-      const data = (await res.json()) as { code: number, data: number }
+      const data = (await res.json()) as {
+        code: number
+        data: { amount: number, claimed: boolean }
+      }
       if (data.code === 0) {
         return data.data
       }
-      return 0
+      return null
     },
   )
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center dark:bg-[linear-gradient(45deg,#d43421,#ff6a0b,#ff5d07)]">
-      <main className="w-full space-y-8">
-        <div className="text-center text-7xl font-bold space-y-2">
+    <div className="min-h-screen flex flex-col items-center pt-36 dark:bg-[linear-gradient(45deg,#d43421,#ff6a0b,#ff5d07)]">
+      <main className="w-full space-y-8 text-center">
+        <div className="text-7xl font-bold space-y-2">
           <p>JOIN TO SHARE</p>
           <p className="text-power-orange dark:text-white">20,000,000</p>
           <p className="text-power-orange dark:text-white">$POWER</p>
           <p>AIRDROP</p>
         </div>
-        <p className="text-center text-2xl">
-          {status === 'authenticated'
-            ? `You are eligible to receive ${amount} $POWER`
+        <p className="text-2xl">
+          {status === 'authenticated' && data
+            ? data.claimed
+              ? 'You have already claimed your airdrop'
+              : `You are eligible to receive ${data.amount} $POWER`
             : 'Sign in to claim your airdrop'}
         </p>
+        {data?.claimed ? (
+          <Button>Share on X</Button>
+        ) : (
+          <Button
+            onClick={async () => {
+              const res = await fetch(
+                `${env.NEXT_PUBLIC_API_URL}/wallets/airdrop`,
+                {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-Token': await getCsrfToken(),
+                  },
+                  credentials: 'include',
+                },
+              )
+              const data = await res.json() as { code: number, data: { transactionHash: string } }
+              if (data.code === 0) {
+                toast.success('Airdrop claimed successfully')
+              }
+              else {
+                toast.error('Failed to claim airdrop')
+              }
+            }}
+          >
+            Claim Airdrop
+          </Button>
+        )}
         <AuthButton className="justify-center" />
+        <Toaster />
       </main>
     </div>
   )
