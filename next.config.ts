9d2927c6eaca45b52c401a436d0296f9c20d280f
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-11-01 11:26:56 +0800

    chore: update deps

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import './src/env'
+
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+        port: '',
+      },
+    ],
+  },
+}
+
+export default nextConfig
