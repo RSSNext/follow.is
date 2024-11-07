72702340e28eacda71b9013d0c78df3fe5d94c46
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-11-07 17:31:46 +0800

    feat: open panel

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
 'use client'
-
-import { authConfigManager, SessionProvider } from '@hono/auth-js/react'
+import { authConfigManager, SessionProvider, useSession } from '@hono/auth-js/react'
+import { OpenPanelComponent } from '@openpanel/nextjs'
 
 import { env } from '@/env'
 
@@ -10,9 +10,24 @@ authConfigManager.setConfig({
   credentials: 'include',
 })
 
+function OpenPanel() {
+  const { data } = useSession()
+  return (
+    <OpenPanelComponent
+      clientId={env.NEXT_PUBLIC_OPENPANEL_CLIENT_ID}
+      apiUrl={env.NEXT_PUBLIC_OPENPANEL_API_URL}
+      trackScreenViews={true}
+      trackAttributes={true}
+      trackOutgoingLinks={true}
+      profileId={data?.user?.id}
+    />
+  )
+}
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
+      <OpenPanel />
       {children}
     </SessionProvider>
   )
--- src/env.ts
+++ src/env.ts
@@ -6,9 +6,13 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_API_URL: z.string().min(1).default('http://localhost:3000'),
     NEXT_PUBLIC_WEB_URL: z.string().min(1).default('http://localhost:2222'),
+    NEXT_PUBLIC_OPENPANEL_API_URL: z.string().min(1),
+    NEXT_PUBLIC_OPENPANEL_CLIENT_ID: z.string().min(1),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_WEB_URL: process.env.NEXT_PUBLIC_WEB_URL,
+    NEXT_PUBLIC_OPENPANEL_API_URL: process.env.NEXT_PUBLIC_OPENPANEL_API_URL,
+    NEXT_PUBLIC_OPENPANEL_CLIENT_ID: process.env.NEXT_PUBLIC_OPENPANEL_CLIENT_ID,
   },
 })
