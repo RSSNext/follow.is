6bd9ed1f43bd35cf40b9c3dba2f8287a1b0764b9
DIYgod <i@diygod.me>
2024-12-22 14:33:25 +0800

    feat: update nav

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -22,7 +22,7 @@ export const siteInfo = {
     { title: 'Features', href: '/#features' },
     { title: 'Testimonials', href: '/#testimonials' },
     { title: 'FAQ', href: '/#faq' },
-    { title: 'AirDrop', href: '/airdrop' },
+    { title: '$POWER', href: 'https://power.top' },
   ],
 } as const
 
