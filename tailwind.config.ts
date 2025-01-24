a1bf48067439fd68ca0085af9fdb2c6012c26e1c
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-01-24 22:15:55 +0800

    feat: css config first

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,8 @@
-import { iconsPlugin } from '@egoist/tailwindcss-icons'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
   plugins: [
-    iconsPlugin({ scale: 1.5 }),
     require('tailwindcss-animate'),
-    require('@tailwindcss/typography'),
   ],
 }
 
