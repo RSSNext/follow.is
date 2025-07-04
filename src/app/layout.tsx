e78490c0a9183161dcdd56a2fb2da230238c1de3
Innei <tukon479@gmail.com>
2025-07-04 14:46:16 +0800

    chore: add ga tag
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,13 +31,27 @@ export default async function RootLayout({
     <html lang="en" className="font-sans antialiased scroll-smooth h-full">
       <head>
         <meta name="apple-itunes-app" content="app-id=6739802604" />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DZMBZBW3EC"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DZMBZBW3EC');
+`,
+          }}
+        />
       </head>
       <Providers>
         <body className="h-full">
           <Header />
-          <GitHubStarProvider stars={stars}>
-            {children}
-          </GitHubStarProvider>
+          <GitHubStarProvider stars={stars}>{children}</GitHubStarProvider>
           <Footer />
         </body>
       </Providers>
